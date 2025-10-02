dedf48a97ccd6a8015e767bb485e6aae0db2f725
José Marcello <josemarcellogardeldealemar@gmail.com>
2025-10-02 20:33:25 -0300

    Alteração do Environments.Prod

--- src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.prod.ts
+++ src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // Localização: src/environments/environment.prod.ts
+
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5288/', // Sua URL base da API de produção
-  imagesUrl: '/assets/images/' // <<-- NOVO: URL base para imagens estáticas do frontend -->>
+  apiUrl: 'https://bolaoonline-testes3.azurewebsites.net' // <-- A URL CORRETA DO BACKEND
 };
