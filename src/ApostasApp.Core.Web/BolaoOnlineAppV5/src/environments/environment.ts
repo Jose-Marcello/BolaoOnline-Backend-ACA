e8b411b6869853f73845e58bf179bc4fec861dad
José Marcello <josemarcellogardeldealemar@gmail.com>
2025-10-03 22:57:20 -0300

    Nova alteração no web.config

--- src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.ts
+++ src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.ts
@@ -1,4 +1,5 @@
     // Localização: src/environments/environment.ts
+    /*
     export const environment = {
       production: false,
       // <<-- ATENÇÃO: SEMPRE SEM '/api' E SEM A BARRA FINAL AQUI -->>
@@ -7,4 +8,11 @@
       // Por exemplo: 'https://seubackend.com' ou 'http://localhost:5000'
       imagesUrl: '/assets/images/'
     }; 
-     
+     */
+    
+    export const environment = {
+  production: false,
+  // Altere a URL da API para o seu App Service no Azure
+  apiUrl: 'http://bolaoonline-testes4.azurewebsites.net', 
+  imagesUrl: 'http://bolaoonline-testes4.azurewebsites.net' 
+};
