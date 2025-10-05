12897bd5a11e3d371dce62931fd09813bbe99ebc
José Marcello <josemarcellogardeldealemar@gmail.com>
2025-10-05 15:10:29 -0300

    Acerto no environment.prod

--- src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.prod.ts
+++ src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://bolaoonline-testes4.azurewebsites.net',
-  imagesUrl: 'https://bolaoonline-testes4.azurewebsites.net'  // <-- A URL CORRETA DO BACKEND
+
+  apiUrl: 'https://app.palpitesbolao.com.br/api',
+  imagesUrl: '/assets/images/'
+
+  //apiUrl: 'https://bolaoonline-testes4.azurewebsites.net',
+  //imagesUrl: 'https://bolaoonline-testes4.azurewebsites.net'  // <-- A URL CORRETA DO BACKEND
+
 };
