ecf08e543df2486ed7e43f4051dcae249f4339e3
José Marcello <josemarcellogardeldealemar@gmail.com>
2025-10-05 13:23:09 -0300

    Novo acerto environments/package

--- src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.ts
+++ src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.ts
@@ -4,11 +4,11 @@
     export const environment = {
 //  production: false,
   // Altere a URL da API para o seu App Service no Azure
- // apiUrl: 'https://app.palpitesbolao.com.br/api',
- // imagesUrl: '/assets/images/'
+  apiUrl: 'https://app.palpitesbolao.com.br/api',
+  imagesUrl: '/assets/images/'
 
-  apiUrl: 'http://bolaoonline-testes4.azurewebsites.net', 
-  imagesUrl: 'http://bolaoonline-testes4.azurewebsites.net' 
+ // apiUrl: 'http://bolaoonline-testes4.azurewebsites.net', 
+  //imagesUrl: 'http://bolaoonline-testes4.azurewebsites.net' 
 
 
 };
