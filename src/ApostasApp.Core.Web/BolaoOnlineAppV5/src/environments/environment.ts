a3541eaf3e76e3b9460b6dc1c0779243b2452253
José Marcello <josemarcellogardeldealemar@gmail.com>
2025-10-05 12:33:35 -0300

    Acerto do Enviroments

--- src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.ts
+++ src/ApostasApp.Core.Web/BolaoOnlineAppV5/src/environments/environment.ts
@@ -1,18 +1,12 @@
-    // Localização: src/environments/environment.ts
-    /*
-    export const environment = {
-      production: false,
-      // <<-- ATENÇÃO: SEMPRE SEM '/api' E SEM A BARRA FINAL AQUI -->>
-      apiUrl: 'http://localhost:5288',
-      // Certifique-se de que esta URL base corresponde ao seu backend
-      // Por exemplo: 'https://seubackend.com' ou 'http://localhost:5000'
-      imagesUrl: '/assets/images/'
-    }; 
-     */
+// <<-- CORREÇÃO: ADICIONE O DOMÍNIO DA API -->>
+  
 
     export const environment = {
-  production: false,
+//  production: false,
   // Altere a URL da API para o seu App Service no Azure
-  apiUrl: 'http://bolaoonline-testes4.azurewebsites.net', 
-  imagesUrl: 'http://bolaoonline-testes4.azurewebsites.net' 
-};
+  apiUrl: 'https://app.palpitesbolao.com.br/api',
+  imagesUrl: '/assets/images/'
+
+//  apiUrl: 'http://bolaoonline-testes4.azurewebsites.net', 
+//  imagesUrl: 'http://bolaoonline-testes4.azurewebsites.net' 
+//};
