1fb7df306d2d3d6d2970a54d080959f4523c7c5b
Jose-Marcello <josemarcellogardeldealemar@gmail.com>
2025-11-21 13:37:57 -0300

    Nova alteração para disparar PULL REQUEST

--- src/ApostasApp.Core.Application/DTOs/Usuarios/ForgotPasswordRequestDto.cs
+++ src/ApostasApp.Core.Application/DTOs/Usuarios/ForgotPasswordRequestDto.cs
@@ -6,7 +6,7 @@ namespace ApostasApp.Core.Application.DTOs.Usuarios
   public class ForgotPasswordRequestDto
   {
     [Required(ErrorMessage = "O Email é obrigatório.")]
-    [EmailAddress(ErrorMessage = "O Email está em formato inválido.")]
+    [EmailAddress(ErrorMessage = "O Email está em formato inválido.")] 
     // CORREÇÃO CRÍTICA: Força o nome da propriedade JSON para 'email' (camelCase)
     // Isso resolve a falha de desserialização no ambiente de produção/ACA.
     [JsonPropertyName("email")]
