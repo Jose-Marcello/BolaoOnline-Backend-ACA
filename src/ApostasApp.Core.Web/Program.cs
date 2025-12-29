90a4bd65b5d9a2fb0c3e439c9a952a3ba300ea92
Jose-Marcello <josemarcellogardeldealemar@gmail.com>
2025-12-29 13:41:26 -0300

    Ajustes REPOSITÓRIO

--- src/ApostasApp.Core.Web/Program.cs
+++ src/ApostasApp.Core.Web/Program.cs
@@ -37,7 +37,7 @@ using System.Text.Json.Serialization;
 
 
 //ALTERAÇÃO PARA DISPARAR PIPE
-var empurraPipe = true;
+var empurraPipe = true; // (true);
 
 System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
